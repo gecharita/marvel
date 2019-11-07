829d9f77ecba8a58fce10a0833a252ecaafdba24
George Haris <gecharita@gmail.com>
2019-11-07 11:43:06 +0200

    Code clearance

--- config/properties.go
+++ config/properties.go
@@ -1,17 +1,12 @@
 package config
 
 import (
-	"log"
-	"os"
-
 	"github.com/magiconair/properties"
 )
 
-// var generalProperties = properties.MustLoadFile("${HOME}/go/src/marvel/config/general.properties", properties.UTF8)
 var generalProperties = properties.MustLoadFile("./general.properties", properties.UTF8)
 
 // GetGeneralProperty generic method to extract property value
 func GetGeneralProperty(property string) string {
-	log.Println(os.Getwd())
 	return generalProperties.MustGet(property)
 }
